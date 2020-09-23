6c7face62b2490ceddbb1f355a400395be645203
Johanna Antonelli <40665145+johantonelli@users.noreply.github.com>
2020-09-23 11:20:10 +0100

    Create crux-tutorial.clj

---
+++ crux-tutorial.clj
@@ -0,0 +1,12 @@
+(ns crux-tutorial.core
+  (:require [crux.api :as crux]))
+
+(def node (crux/start-node {}))
+
+(def manifest
+  {:crux.db/id :manifest
+   :pilot-name "Johanna"
+   :id/rocket "SB002-sol"
+   :id/employee "22910x2"
+   :badges "SETUP"
+   :cargo ["stereo" "gold fish" "slippers" "secret note"]})
